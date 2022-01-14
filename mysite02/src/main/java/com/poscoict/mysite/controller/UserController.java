84f9c01f6a3db20b115888226d8d003fa24d9194
user <user@DESKTOP-0E1JB75>
2022-01-14 15:10:05 +0900

    mysite02 #3

--- mysite02/src/main/java/com/poscoict/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/poscoict/mysite/controller/UserController.java
@@ -5,21 +5,20 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.poscoict.mysite.mvc.user.UserActionFactory;
+import com.poscoict.web.mvc.Action;
+import com.poscoict.web.mvc.ActionFactory;
 
 public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		
 		String actionName = request.getParameter("a");
-		if ("joinform".equals(actionName)) {
-			
-		} else if ("join".equals(actionName)) {
-			
-		} else {
-			
-		}
+		
+		ActionFactory af = new UserActionFactory();
+		Action action = af.getAction(actionName);
+		action.execute(request, response);
 		
 	}
 
---
+++ mysite02/src/main/java/com/poscoict/mysite/mvc/user/UserActionFactory.java
@@ -0,0 +1,13 @@
+package com.poscoict.mysite.mvc.user;
+
+import com.poscoict.web.mvc.Action;
+import com.poscoict.web.mvc.ActionFactory;
+
+public class UserActionFactory extends ActionFactory {
+
+	@Override
+	public Action getAction(String actionName) {
+		return null;
+	}
+
+}
