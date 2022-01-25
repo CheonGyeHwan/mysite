ff0357c5106ff2483ca76cc1aa6e519640cbed29
user <user@DESKTOP-0E1JB75>
2022-01-25 11:38:48 +0900

    mysite03 - exception#finish

--- mysite03/src/main/java/com/poscoict/mysite/controller/UserController.java
+++ mysite03/src/main/java/com/poscoict/mysite/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.poscoict.mysite.controller;
 
 import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -91,4 +90,9 @@ public class UserController {
 		
 		return "redirect:/user/update";
 	}
+	
+//	@ExceptionHandler(Exception.class)
+//	public String UserControllerExceptionHandler() {
+//		return "error/exception";
+//	}
 }
