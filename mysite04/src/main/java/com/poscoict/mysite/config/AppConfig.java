b75829800261ef97d0d329c5368b6bc229e71256
CheonGyeHwan <cgh5531@naver.com>
2022-02-09 12:46:04 +0900

    mysite04 #4

--- mysite04/src/main/java/com/poscoict/mysite/config/AppConfig.java
+++ mysite04/src/main/java/com/poscoict/mysite/config/AppConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
-
 import com.poscoict.config.app.DBConfig;
 import com.poscoict.config.app.MyBatisConfig;
 
